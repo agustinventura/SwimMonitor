ce9b1b77ef48ba01ba182101e04dad900e24660d
Agustín Ventura <agustinventura@gmail.com>
2017-11-22 15:24:00 +0100

    adds style to list

--- js/swimMonitor.js
+++ js/swimMonitor.js
@@ -1,12 +1,24 @@
+StyleEnum = {
+	BRAZA: "Pecho",
+	CROL: "Crol",
+	ESPALDA: "Espalda",
+	MARIPOSA: "Mariposa"
+}
+
 function init() {
+	hideNonVisibleDivs();
+    setInitialListeners();
+}
+
+function hideNonVisibleDivs() {
 	$("#styleSelector").hide();
 	$("#oldTrainings").hide();
-    setInitialListeners();
 }
 
 function setInitialListeners() {
 	$("#showStyles").click(showStyles);
     $("#showOldTrainings").click(showOldTrainings);
+	$("#chooseStyle li").click(styleChosen);
     
     /*$("#resume").click(resumeWorkout);
     $("#exit").click(exit);
@@ -35,12 +47,25 @@ function exit() {
 
 function showStyles() {
 	$("#initialScreen").hide();
+	createStylesList();
 	$("#styleSelector").show();
+}
+
+function createStylesList() {
+	var stylesList = $("chooseStyle");
+	stylesList.clear();
+	
 }
 
 function showOldTrainings() {
 	$("#initialScreen").hide();
 	$("#showOldTrainings").show();
+}
+
+function styleChosen() {
+	var index = $(this).index();
+    var text = $(this).text();
+    alert('Index is: ' + index + ' and text is ' + text);
 }
 
 $(document).ready(init);
