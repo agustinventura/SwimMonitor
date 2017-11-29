fe61baf2b5b8ae2bee24e83ec32ecdd83d9dd6e9
Agustín Ventura <agustinventura@gmail.com>
2017-11-29 14:28:53 +0100

    adds localstorage and previous trainings screens

--- js/swimMonitor.js
+++ js/swimMonitor.js
@@ -10,15 +10,8 @@ LengthEnum = {
 	50: "50m"
 }
 
-var selectedStyle = null;
-var selectedLength = null;
-var lengthCount = 1;
 var totalTimer = null;
 var lengthTimer = null;
-var totalSeconds = null;
-var lengthSeconds = null;
-var minLengthTime = null;
-var maxLengthTime = null;
 
 function init() {
 	hideNonVisibleDivs();
@@ -60,20 +53,32 @@ function backPressed(e) {
 }
 
 function reset() {
-	lengthCount = 1;
-	totalTimer = null;
+	training = null;
+	clearInterval(lengthTimer);
+	clearInterval(totalTimer);
 	lengthTimer = null;
-	totalSeconds = null;
-	lengthSeconds = null;
-	minLengthTime = null;
-	maxLengthTime = null;
+	totalTimer = null;
 }
 
 function exit() {
     tizen.application.getCurrentApplication().exit();
 }
 
+function getNewTraining() {
+	var training = {
+		selectedStyle: null,
+		selectedLength: null,
+		trainingDate: null,
+		lengthCount: 1,
+		totalSeconds: null,
+		lengthSeconds: null,
+		minLengthTime: null,
+		maxLengthTime: null
+	};
+	return training;
+}
 function showStyles() {
+	training = getNewTraining();
 	$("#initialScreen").hide();
 	createStylesList();
 	setClickListener($(".style"), showLength);
@@ -91,17 +96,54 @@ function createStylesList() {
 	for (var style in StyleEnum) {
 		stylesList.append($('<li>').text(StyleEnum[style]).addClass('style'));
 	}
-	stylesList.attr('size', Object.keys(StyleEnum).length);
 }
 
 function showPreviousTrainings() {
 	$("#initialScreen").hide();
-	$("#showPreviousTrainings").show();
+	$("#previousTrainings").show();
+	var trainingsList = $("#trainings");
+	trainingsList.empty();
+	for (var i = 0; i < localStorage.length; i++) {
+		trainingsList.append($('<li>').text(formatDate(new Date(localStorage.key(i)))).addClass('trainingItem'));
+	}
+	setClickListener($("#exitPreviousTrainings"), exitPreviousTrainings);
+	setClickListener($(".trainingItem"), showPreviousTraining);
+}
+
+function exitPreviousTrainings() {
+	$("#previousTrainings").hide();
+	$("#initialScreen").show();
+}
+
+function showPreviousTraining() {
+	var index = $(this).index();
+	var trainingDate = localStorage.key(index);
+	training = localStorage.getItem(trainingDate);
+	$("#previousTrainings").hide();
+	$("#previousTraining").show();
+	$("#sumUpDate").html(formatDate(new Date(training.selectedStyle)));
+	$("#sumUpStyle").html(StyleEnum[training.selectedStyle]);
+	$("#sumUpLength").html(LengthEnum[training.selectedLength]);
+	$("#sumUpTime").html(getFormattedTime(training.totalSeconds));
+	$("#totalMeters").html(training.selectedLength*training.lengthCount);
+	$("#bestTime").html(getFormattedTime(training.minLengthTime));
+	$("#worstTime").html(getFormattedTime(training.maxLengthTime));
+	$("#averageTime").html(getFormattedTime(training.totalSeconds/training.lengthCount));
+	setClickListener($("#exitPreviousTraining"), exitPreviousTraining);
+}
+
+function exitPreviousTraining() {
+	training = null;
+	showPreviousTrainings();
+}
+
+function formatDate(date) {
+  return date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
 }
 
 function showLength() {
 	var index = $(this).index();
-    selectedStyle = Object.keys(StyleEnum)[index];
+    training.selectedStyle = Object.keys(StyleEnum)[index];
 	$("#styleSelector").hide();
 	createLengthList();
 	setClickListener($(".length"), showTrainingReady);
@@ -114,12 +156,11 @@ function createLengthList() {
 	for (var length in LengthEnum) {
 		lengthsList.append($('<li>').text(LengthEnum[length]).addClass('length'));
 	}
-	lengthsList.attr('size', Object.keys(LengthEnum).length);
 }
 
 function showTrainingReady() {
 	var index = $(this).index();
-    selectedLength = Object.keys(LengthEnum)[index];
+    training.selectedLength = Object.keys(LengthEnum)[index];
 	$("#lengthSelector").hide();
 	$("#trainingReady").show();
 	setClickListener($("#startTraining"), showCurrentTraining);
@@ -128,26 +169,27 @@ function showTrainingReady() {
 function showCurrentTraining() {
 	$("#trainingReady").hide();
 	$("#currentTraining").show();
+	training.trainingDate = new Date();
 	setClickListener($("#addLength"), newLength);
 	setClickListener($("#pauseTraining"), pauseTraining);
-	$("#selectedStyle").html(StyleEnum[selectedStyle]);
-	$("#selectedLength").html(LengthEnum[selectedLength]);
-	$("#lengthCount").text(lengthCount);
-	totalSeconds = 0;
+	$("#selectedStyle").html(StyleEnum[training.selectedStyle]);
+	$("#selectedLength").html(LengthEnum[training.selectedLength]);
+	$("#lengthCount").text(training.lengthCount);
+	training.totalSeconds = 0;
 	totalTimer = setInterval(function () {
             refreshTotalSeconds();
         }, 1000);
-	lengthSeconds = 0;
+	training.lengthSeconds = 0;
 	lengthTimer = setInterval(function () {
             refreshLengthSeconds();
         }, 1000);
-	minLengthTime = Number.MAX_VALUE;
-	maxLengthTime = Number.MIN_VALUE;
+	training.minLengthTime = Number.MAX_VALUE;
+	training.maxLengthTime = Number.MIN_VALUE;
 }
 
 function refreshTotalSeconds() {
-	totalSeconds++;
-	$("#totalTime").text(getFormattedTime(totalSeconds));
+	training.totalSeconds++;
+	$("#totalTime").text(getFormattedTime(training.totalSeconds));
 }
 
 function getFormattedTime(seconds) {
@@ -174,22 +216,22 @@ function getFormattedTime(seconds) {
 }
 
 function refreshLengthSeconds() {
-	lengthSeconds++;
-	$("#lengthTime").text(getFormattedTime(lengthSeconds));
+	training.lengthSeconds++;
+	$("#lengthTime").text(getFormattedTime(training.lengthSeconds));
 }
 
 function newLength() {
-	lengthCount++;
-	$("#lengthCount").text(lengthCount);
-	if (lengthSeconds < minLengthTime) {
-		minLengthTime = lengthSeconds;
+	training.lengthCount++;
+	$("#lengthCount").text(training.lengthCount);
+	if (training.lengthSeconds < training.minLengthTime) {
+		training.minLengthTime = training.lengthSeconds;
 	}
-	if (lengthSeconds > maxLengthTime) {
-		maxLengthTime = lengthSeconds;
+	if (training.lengthSeconds > training.maxLengthTime) {
+		training.maxLengthTime = training.lengthSeconds;
 	}
 	clearInterval(lengthTimer);
-	lengthSeconds = 0;
-	$("#lengthTime").text(getFormattedTime(lengthSeconds));
+	training.lengthSeconds = 0;
+	$("#lengthTime").text(getFormattedTime(training.lengthSeconds));
 	lengthTimer = setInterval(function () {
             refreshLengthSeconds();
         }, 1000);
@@ -219,18 +261,19 @@ function resumeTraining() {
 
 function endTraining() {
 	if (lengthCount === 1) {
-		minLengthTime = lengthSeconds;
-		maxLengthTime = lengthSeconds;
+		training.minLengthTime = lengthSeconds;
+		training.maxLengthTime = lengthSeconds;
 	}
+	localStorage.setItem(training.trainingDate, training);
 	$("#pausedTraining").hide();
 	$("#trainingSumUp").show();
-	$("#sumUpStyle").html(StyleEnum[selectedStyle]);
-	$("#sumUpLength").html(LengthEnum[selectedLength]);
-	$("#sumUpTime").html(getFormattedTime(totalSeconds));
-	$("#totalMeters").html(selectedLength*lengthCount);
-	$("#bestTime").html(getFormattedTime(minLengthTime));
-	$("#worstTime").html(getFormattedTime(maxLengthTime));
-	$("#averageTime").html(getFormattedTime(totalSeconds/lengthCount));
+	$("#sumUpStyle").html(StyleEnum[training.selectedStyle]);
+	$("#sumUpLength").html(LengthEnum[training.selectedLength]);
+	$("#sumUpTime").html(getFormattedTime(training.totalSeconds));
+	$("#totalMeters").html(training.selectedLength*training.lengthCount);
+	$("#bestTime").html(getFormattedTime(training.minLengthTime));
+	$("#worstTime").html(getFormattedTime(training.maxLengthTime));
+	$("#averageTime").html(getFormattedTime(training.totalSeconds/training.lengthCount));
 	setClickListener($("#exitTraining"), exitTraining);
 }
 
